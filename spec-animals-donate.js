155076e030402942325f8fed0cecb212c85a1ceb
Eyal Pinkas <pinkasey@gmail.com>
2019-06-20 15:25:35 +0300

    wait instead of sleep

--- spec-animals-donate.js
+++ spec-animals-donate.js
@@ -1,18 +1,24 @@
 describe('Animals-Now Donate', function () {
-
-
-  it('Donate', function () {
-    browser.waitForAngularEnabled(false);
-
+  browser.waitForAngularEnabled(false);
+  var EC = protractor.ExpectedConditions;
+  
+  beforeEach(function() {
     browser.get('https://animals-now.org/donate?group=IT');
+  });
 
+  it('Simply press Donate button', function () {
+    
     element(by.css('button.fl-button-submit')).click();
 
-    //TODO: wait for page to reload, instead of doing a sleep
-    browser.sleep(4000);
+    browser.wait(EC.urlContains('secure.cardcom.solutions'), 5000);
 
+    //This is somewhat redundant - the browswer.wait would fail before this fails
     expect(browser.getCurrentUrl())
       .toContain('https://secure.cardcom.solutions/External/lowProfileClearing/46443.aspx');
+
+    //TODO:
+    //verify amount is right, no HOK text present, etc.
+
   });
 
 });
