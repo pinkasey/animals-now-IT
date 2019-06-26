a0dee98819b4dd6667095cd7dff1c8435ec14c7b
Eyal Pinkas <pinkasey@gmail.com>
2019-06-26 19:02:39 +0300

    run with headless chrome

--- conf-animals.js
+++ conf-animals.js
@@ -1,4 +1,13 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec-animals-donate.js']
+  specs: ['spec-animals-donate.js'],
+
+  capabilities: {
+    browserName: 'chrome',
+  
+    chromeOptions: {
+       args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+     }
+  }  
+
 };
